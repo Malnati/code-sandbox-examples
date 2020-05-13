670a64362470456427e1f7add566e1d8fc5dcf04
Malnati <ricardomalnati@gmail.com>
2020-05-13 16:08:24 -0300

    Aligning victory dependency.

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,23 +1,45 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+import BootstrapContainer from 'react-bootstrap/Container';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { RadialChart } from 'react-vis';
+import { VictoryPie } from 'victory';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/react-vis/dist/style.css';
-
-import { RadialChart } from 'react-vis';
 import './App.css';
 
+class PieChart extends Component {
+  render() {
+    return <VictoryPie />;
+  }
+}
+
 const reportData = [
-  { name: 'Gustavo Da Silva Sauro', goal: 5, done: 5 },
-  { name: 'Renato', goal: 3, done: 7 },
-  { name: 'Rafael Malnati Rosa Lima', goal: 8, done: 2 },
+  {
+    name: 'Gustavo Da Silva Sauro',
+    goal: 10,
+    done: 5,
+    radialAngle: 3,
+    radialRadius: 10,
+  },
+  {
+    name: 'Renato dos Santos',
+    goal: 9,
+    done: 3,
+    radialAngle: 13,
+    radialRadius: 10,
+  },
+  {
+    name: 'Rafael Malnati Rosa Lima',
+    goal: 8,
+    done: 2,
+    radialAngle: 13,
+    radialRadius: 10,
+  },
 ];
 
 const Rows = (props) => {
@@ -28,46 +50,32 @@ const Rows = (props) => {
       <Col>{item.done}</Col>
       <Col>
         <RadialChart
-          data={[{ angle: item.done }, { angle: item.goal }]}
-          width={50}
-          height={50}
+          data={[
+            {
+              angle: item.radialAngle,
+              radius: item.radialRadius,
+              padAngle: 15,
+              subLabel: 'With annotation',
+              label: 'Test',
+            },
+          ]}
+          width={100}
+          height={100}
         />
       </Col>
     </Row>
   ));
 };
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
-
-  return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
-  );
-};
-
 const App = () => (
-  <Container className="p-3">
-    <Jumbotron>
-      <h1 className="header">Testing Rows</h1>
-      <Container fluid>
-        <Rows report={reportData} />
-      </Container>
-      <ExampleToast>
-        Example Toast
-        <span role="img" aria-label="tada">
-          . . 🎉
-        </span>
-      </ExampleToast>
-    </Jumbotron>
-  </Container>
+  <BootstrapContainer className="p-3">
+    <h1 className="header">Report by ReactVis</h1>
+    <BootstrapContainer fluid>
+      <Rows report={reportData} />
+    </BootstrapContainer>
+    <h1 className="header">Report by Victory</h1>
+    <PieChart />
+  </BootstrapContainer>
 );
 
 export default App;
