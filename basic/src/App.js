e8ca3d727b8f521e9c3b47ab2e95988ab6109b43
Malnati <ricardomalnati@gmail.com>
2020-05-13 00:32:06 -0300

    Adding chrat to basic.

--- basic/src/App.js
+++ basic/src/App.js
@@ -5,7 +5,38 @@ import Toast from 'react-bootstrap/Toast';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import '../node_modules/react-vis/dist/style.css';
+
+import { RadialChart } from 'react-vis';
 import './App.css';
+
+const reportData = [
+  { name: 'Gustavo', goal: 10, done: 5 },
+  { name: 'Renato', goal: 10, done: 5 },
+  { name: 'Rafael', goal: 10, done: 5 },
+];
+
+const Rows = (props) => {
+  return props.report.map((item) => (
+    <>
+      <Row>
+        <Col>{item.name}</Col>
+        <Col>{item.goal}</Col>
+        <Col>{item.done}</Col>
+        <Col>
+          <RadialChart
+            data={[{ angle: item.done }, { angle: item.goal }]}
+            width={50}
+            height={50}
+          />
+        </Col>
+      </Row>
+    </>
+  ));
+};
 
 const ExampleToast = ({ children }) => {
   const [show, toggleShow] = useState(true);
@@ -26,12 +57,15 @@ const ExampleToast = ({ children }) => {
 const App = () => (
   <Container className="p-3">
     <Jumbotron>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
+      <h1 className="header">Welcome parcel goals report!</h1>
       <ExampleToast>
-        We now have Toasts
+        Report
         <span role="img" aria-label="tada">
-          🎉
+          . . 🎉
         </span>
+        <Container fluid>
+          <Rows report={reportData} />
+        </Container>
       </ExampleToast>
     </Jumbotron>
   </Container>
