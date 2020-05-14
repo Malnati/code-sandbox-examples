b2f1f215cabbd839d216a31b9b98172755fbdc1c
Malnati <ricardomalnati@gmail.com>
2020-05-13 21:36:28 -0300

    Victory PieChart fixed.

--- basic/src/App.js
+++ basic/src/App.js
@@ -5,63 +5,68 @@ import BootstrapContainer from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import { RadialChart } from 'react-vis';
 import { VictoryPie } from 'victory';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/react-vis/dist/style.css';
 import './App.css';
 
-class PieChart extends Component {
-  render() {
-    return <VictoryPie />;
-  }
-}
-
-const reportData = [
+const parcels = [
   {
     name: 'Gustavo Da Silva Sauro',
     goal: 10,
     done: 5,
-    radialAngle: 3,
-    radialRadius: 10,
+    left: 5,
   },
   {
     name: 'Renato dos Santos',
     goal: 9,
     done: 3,
-    radialAngle: 13,
-    radialRadius: 10,
+    left: 6,
   },
   {
     name: 'Rafael Malnati Rosa Lima',
     goal: 8,
     done: 2,
-    radialAngle: 13,
-    radialRadius: 10,
+    left: 6,
   },
 ];
 
-const Rows = (props) => {
-  return props.report.map((item) => (
+const calc = () => {
+  return parcels.map((data) => {
+    const sideA = {
+      x: 'Done',
+      y: data.done,
+    };
+    const blueB = {
+      x: 'Left',
+      y: data.left,
+    };
+    const containter = [sideA, blueB];
+    data.chart = { containter };
+    return data;
+  });
+};
+
+class Slices extends Component {
+  constructor(_props) {
+    super();
+    this.chart = _props.chart;
+  }
+
+  render() {
+    return <VictoryPie data={this.chart.containter} />;
+  }
+}
+
+const GridReport = (props) => {
+  return props.report.map((parcel) => (
     <Row className=".flex-row align-items-center">
-      <Col>{item.name}</Col>
-      <Col>{item.goal}</Col>
-      <Col>{item.done}</Col>
+      <Col>{parcel.name}</Col>
+      <Col>{parcel.goal}</Col>
+      <Col>{parcel.done}</Col>
       <Col>
-        <RadialChart
-          data={[
-            {
-              angle: item.radialAngle,
-              radius: item.radialRadius,
-              padAngle: 15,
-              subLabel: 'With annotation',
-              label: 'Test',
-            },
-          ]}
-          width={100}
-          height={100}
-        />
+        <Slices chart={parcel.chart} />
       </Col>
     </Row>
   ));
@@ -69,12 +74,8 @@ const Rows = (props) => {
 
 const App = () => (
   <BootstrapContainer className="p-3">
-    <h1 className="header">Report by ReactVis</h1>
-    <BootstrapContainer fluid>
-      <Rows report={reportData} />
-    </BootstrapContainer>
-    <h1 className="header">Report by Victory</h1>
-    <PieChart />
+    <h1 className="header">Report by Victory in the Grid</h1>
+    <GridReport report={calc()} />
   </BootstrapContainer>
 );
 
